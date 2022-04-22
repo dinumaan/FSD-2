1a3d1ed05eb22db53cb79b189ae2a470471ac1ac
dinumaan <dineshmatrix675@gmail.com>
2022-04-22 16:31:53 +0530

    added file of 21th aprail

---
+++ Week-1/21th-apr.js
@@ -0,0 +1,109 @@
+// // const func = (temp, isRaining) => {
+
+//     // when children go park?
+
+    // if(temp >= 20 && temp <= 25 && isRaining == false){
+    //     console.log("children can play in park");
+    // }else if(temp >= 18 && temp <= 20 && isRaining == true){
+    //     console.log("children can play in school");
+    // }else{
+    //     console.log("not allowed to go outside");
+    // }
+
+// // }
+
+// // let tmp = 21;
+// // let isRaining = true;
+
+// func(21, false);
+
+// Q2. Create a program to print the result as pass if the marks obtained is greater than or equal to 40 using ternary operator.
+
+// const func = (marks)  => { 
+
+//     ( marks >= 40 ) ? console.log("Passed !") : console.log("Sorry Failed !");
+// }
+
+
+// func(45)
+
+// Q 3
+const obj5 = { 
+    realname: "Ram", 
+    country: "India", 
+    city: ["Agra", "Kolkata"], 
+    surname: "singh" 
+};
+
+
+const kool1 = Object.create(obj5) 
+
+kool1.name = "deepak"
+
+kool1.type = "funky"
+
+kool1.behaveiour = "cool"
+
+// console.log(kool1)
+
+// let index = Object.entries(kool1)
+
+// for (let i = 0; i <= Object.keys(kool1).length-1; i++) {
+//     console.log(kool1[index[i][0]])
+// }
+
+// for(let i in kool1){
+//     console.log(kool1[i]);
+// }
+
+// cost => 
+
+// let sometingh  = 0; //  constant
+
+// if(sometingh > 0){
+//     console.log(sometingh);
+// }  // constant
+
+// let  i = 0;
+            // c    c3
+// for( ; i < 700; ++i){
+//     // console.log(i);  // constant  c1
+
+//     // if(i > 50){
+//     //     console.log("i is greate than 50");
+//     // } // c2    
+// }
+
+// 700*(c1 + c2) + c*701 + c3*701
+
+
+
+// 0 
+// 99
+
+// 100
+
+// let i1 = 100
+
+// console.log(++i1); // 101
+// console.log(++i1); // 100
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
