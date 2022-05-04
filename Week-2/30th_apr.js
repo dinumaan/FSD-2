68099543280836173297181f45e96e54a06dfa86
dinumaan <dineshmatrix675@gmail.com>
2022-05-04 08:53:56 +0530

    added class desk file

---
+++ Week-2/30th_apr.js
@@ -0,0 +1,101 @@
+// Hello Everyone class start at 07:03
+
+// let k = [
+//     game =  "basket", 
+//     game2 = "basket1",
+//     game3 = "besket3"
+// ]
+
+// console.log(k)
+
+// console.log(game);
+
+// // everything in JS is obejct 
+
+// // Arrays => Object 
+
+// let arr = [1, 2, 3];
+
+// // arr.flatMap()
+
+// ES6 
+
+// let game = "basket";
+// let game2 = "basket1";
+// let game3 = "basket2";
+
+// game = "cir";
+// ga
+
+// let [game = "basket", ]
+
+// let arr = [1,]
+
+// let [game = "basket", game2 = "basket1", game3 = "besket3", endwe = "c"] = ["cricket"];
+
+
+
+// let [game, game2, game3] = ["circ1", "circ2", "circ3"];
+
+// let =>
+
+// Array => collestions data
+
+// class => collectio of variable
+
+// let 
+
+// int 
+// console.log(k);
+
+// let k = { 
+//     game: "basket",
+//     game2: {
+//         type: "sapio",
+//         type1:"charge"
+//     }, 
+//     game3: "besket3" 
+// }
+
+// let dummy = k.game;
+// let unhaappy = k.game2.type
+// let good  = k.game2.type1
+
+// let { game: dummy, game2:{type:unhappy,type1:good}, } = k
+
+// console.log(k.game);
+
+// let arr = [2, 3, 7, 10, 40,0, 12, 123, 4, 5];
+
+// Hii => 100 times
+
+// for(let i = 0; i <= n; i++){
+    
+//     for(let j = 0; j <= m; j++){
+
+//     }
+// }
+
+// c1 => 101
+// c2 => 101
+// c3 => 100
+
+// Total time = c1*1^9 + c2*1^7 +  c3*1^9;
+
+// Total Time = 3*c*1^7;
+
+// O(n)
+// Asympotic 
+
+//c1 = c2 = c2 = c;
+
+
+
+// const 
+
+
+// insturtion => 0&1's 
+
+// cpu => fetch => decode => excecute => store  => times 
+
+
