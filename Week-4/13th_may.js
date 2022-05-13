b90335695a6bea2b2f39dd8a508b4a121899f984
dinumaan <dineshmatrix675@gmail.com>
2022-05-13 20:22:01 +0530

    added code file of 13th may

---
+++ Week-4/13th_may.js
@@ -0,0 +1,79 @@
+// 1. vivek doubt
+
+// var count=false;
+
+// let countValue = new Promise(function(resolve, reject){
+//     if(count) {
+//         resolve("There is a count value");
+//     }else{
+//       reject("There is no count value ");
+//     }
+// });
+
+// //countValue.then(function successValue(result){
+//   //  console.log(result)
+// //});
+// //console.log (countValue);
+
+// countValue.then(function successValue(result){
+//     console.log(result);
+// })
+// .catch(function successValue(result){
+//     console.log("rejected");
+//     console.log(result);
+// });
+
+
+// 2. tinu doubt
+
+// function nextInLine(arr, item) {
+//     // Only change code below this line
+//     arr.push(item);
+//     const remove = arr.shift();
+//     // arr = [2, 3, 4, 5, 6]
+//     return remove;
+//     // Only change code above this line
+//   }
+  
+//   // Setup
+//   const testArr = [1, 2, 3, 4, 5];
+  
+//   // Display code
+// //   console.log("Before: " + JSON.stringify(testArr)); // [1, 2, 3, 4, 5]
+// //   console.log(nextInLine(testArr, 6));
+// //   console.log("After: " + JSON.stringify(testArr)); // [2, 3, 4, 5, 6]
+
+
+// for(let i = 0; i < 6; i++){
+//     console.log(nextInLine(testArr, 6));
+//     console.log("After: " + JSON.stringify(testArr));
+// }
+
+
+var isValid = function(s) {
+    // if(s[0])
+    
+    console.log(s[0]);
+    
+};
+
+isValid("(){}");
+
+
+// ()[]{}
+// ({})
+
+
+// ([{]})
+
+// {]
+
+// ({[)}
+
+// )
+// }
+// ]
+
+// .......(
+// .......{
+// .......[
