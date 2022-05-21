1c0d630240964a0678112e00b6ddfbc6fc907d35
dinumaan <dineshmatrix675@gmail.com>
2022-05-21 17:38:48 +0530

    added code till 21 may

---
+++ Week-5/20th_may.mjs
@@ -0,0 +1,42 @@
+// uncleared doubt => why code is executing & ES5
+
+// import c, {func} from './file.mjs'
+
+// let {c, func} = require('./file.js');
+
+// console.log(c);
+
+// console.log(func);
+
+// script tag
+
+// 2) how this code return 2nd code first
+
+// import fs from "fs";
+
+
+// const content = "Some more content!";
+
+// let fileName = "./created.txt";
+
+// fs.readFile(fileName, 'utf-8', (err, b) => {
+  
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   console.log(b);
+// });
+
+// fs.writeFile(fileName, content, (err2) => {
+  
+//   if (err2) {
+//     console.log(err2);
+//     return;
+//   }
+
+//   console.log("Wrote some more content!");
+
+// });
+
+
---
+++ Week-5/file.mjs
@@ -0,0 +1,24 @@
+
+// class myClass{
+//     a = 10;
+//     constructor(name){
+//        console.log(`Hii ${name}`)
+//     }
+   
+//     func(params) {
+
+//     }
+// }
+
+// function func(){
+//     let a = 10;
+//     console.log("Hii");
+// }
+
+
+// module.exports = {
+//     myClass: myClass,
+//     func: func
+// }
+
+// console.log("Hello frome deepak doubt !")
